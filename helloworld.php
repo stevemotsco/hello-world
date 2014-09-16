f09e5b73b2e433081549117b745a2a192c53fa5c
Steve Motsco <stephenmotsco@g.harvard.edu>
2014-09-15 19:16:54 -0500

    Updated readme file; added index file.

---
+++ helloworld.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+               <title>Hello World</title>
+               <meta charset='utf-8'>
+                             
+</head>
+<body>
+               <?php
+               echo 'Hello World! You are testing finally in folder ';
+               echo $_SERVER['SERVER_NAME'];
+               ?>
+              
+</body>
+</html>
